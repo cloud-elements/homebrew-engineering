1fd49492d27e53cb1fac68a283879e2b8e7e707c
Rocky Madden <git@rockymadden.com>
2018-11-28 13:37:18 -0700

    fix: update out-of-date version

--- Formula/stopandfix.rb
+++ Formula/stopandfix.rb
@@ -1,6 +1,6 @@
 class Stopandfix < Formula
   homepage "https://github.com/cloud-elements/stopandfix-releases"
-  version "0.1.0"
+  version "0.1.1"
   url "https://github.com/cloud-elements/stopandfix-releases/releases/download/v0.1.1/stopandfix__x86_64-apple-darwin"
   sha256 "79ddb1e9ba0447c1f8c213f5918186306161cf8bc59ceafddf5a761a605c1efd"
 
